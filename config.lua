e57755cfd06b052a8fec89cb78cd2da0fa69ba59
momo.agent <maxanit93@gmail.com>
2025-09-13 15:25:26 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,21 @@
+A = {};
+
+
+A.Arenas = {
+    {
+        id = 1,
+        queueCommand = "heli1v1",
+        UseOwnWeapons = true,
+        weapons = {
+            "weapon_pistol",
+            "weapon_specialcarbine",
+        },
+        spawnPoints = {
+            team1Spawn = vector3(-465.9135, -659.6641, 32.4091),
+            team2Spawn = vector3(-627.4709, -560.9130, 34.9932),
+            buzzardSpawn1 = vector3(-625.6486, -566.6072, 35.0546),
+            buzzardSpawn2 = vector3(-474.5487, -664.1441, 32.5761),
+            SpawnAtEnd = vector3(296.4014, -584.5807, 43.1366),
+        }
+    },
+}
