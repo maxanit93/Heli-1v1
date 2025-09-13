e06d649683e415efcff61bc14fff8ad5437f0221
momo.agent <maxanit93@gmail.com>
2025-09-13 15:25:43 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+
+
+fx_version 'adamant'
+
+game 'gta5'
+
+author "max.anit"
+
+description "Heli 1v1"
+
+version '1.6.0'
+
+server_scripts {
+	'server.lua'
+}
+
+shared_scripts  {
+	'config.lua'
+}
+
+client_scripts {
+	'client.lua'
+}
